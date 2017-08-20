4468792ac7ba7e4b402e1fcf7c756b5f627530e5
Ben Corrie <bcorrie@vmware.com>
2017-08-20 12:33:44 -0700

    Create runtest.sh

---
+++ dockerfile/ENV/runtest.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+output=$(docker run bensdoings/vch-test:master /bin/bash -c export) 
+matches=$(printf "$output" | grep 'myCat="fluffy"\|myCat2="fluffy"\|myDog="Rex The Dog"\|myDog2="Rex The Dog"\|myName="John Doe"\|myName2="John Doe"' | wc -l)
+if [[ $matches == 6 ]]; then 
+   echo "PASSED"
+   exit 0
+else
+   echo "FAILED: Output: "
+   printf "$output"
+   exit 1
+fi
