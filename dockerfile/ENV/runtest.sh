6fc03b2d3d2ec2d8ac79ec67ef7f6bb9559fc63c
Ben Corrie <bcorrie@vmware.com>
2017-08-20 13:08:37 -0700

    Update runtest.sh

--- dockerfile/ENV/runtest.sh
+++ dockerfile/ENV/runtest.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 output=$(export) 
-matches=$(printf "$output" | grep 'myCat="fluffy"\|myCat2="fluffy"\|myDog="Rex The Dog"\|myDog2="Rex The Dog"\|myName="John Doe"\|myName2="John Doe"' | wc -l)
+matches=$(printf '$output' | grep 'myCat="fluffy"\|myCat2="fluffy"\|myDog="Rex The Dog"\|myDog2="Rex The Dog"\|myName="John Doe"\|myName2="John Doe"' | wc -l)
 if [[ $matches == 6 ]]; then 
    echo "PASSED"
    exit 0
 else
    echo "FAILED: Output: "
-   printf "$output"
+   printf '$output'
    exit 1
 fi
